9629ab109714ad3452442914dd2dbccbbb6cc673
SamLeBlanc <83605234+SamLeBlanc@users.noreply.github.com>
2021-05-06 16:37:32 -0700

    update file path

--- scripts/dataLoadingAndManipulation.js
+++ scripts/dataLoadingAndManipulation.js
@@ -1,6 +1,6 @@
 function loadDataFromCSV(){
   console.log('Loading data, please wait...')
-  d3.csv("/data/full3.csv").then(function(data) {
+  d3.csv("full3.csv").then(function(data) {
     keys = Object.keys(data[0])
     data.forEach(function(d) {
       keys.forEach(function(k) { if (k != 'GEOID10' && k != 'SIZE') d[k] = +d[k] })
